0591341e3382f15db89890822fb0b794904a24dd
Ekaterina <eshirkevich@gmail.com>
2022-08-08 15:58:03 +0300

    Examole013_factorial

---
+++ Example013_RecursionAlgorithm/factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿// написать метод, кот. принимает число и возвращает его факториал
+
+double Factorial(int n)
+{
+    if(n==1)
+    return 1;
+    else 
+    return n*Factorial(n-1);
+}
+for(int i=1; i<40; i++) // смотрим факт-лы от 1 до 39 например
+Console.WriteLine($"{i}!={Factorial(i)}");
